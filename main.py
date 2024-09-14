06a4b94c84c6bcc9647fdaea2c483c9b3acf8a64
pisarpe1 <petr.pisarik@volnz.cz>
2024-09-14 19:29:20 +0200

    feat: https://stackoverflow.com/questions/22583391/peak-signal-detection-in-realtime-timeseries-data/43512887#43512887

--- main.py
+++ main.py
@@ -3,9 +3,41 @@ import pathlib
 
 import matplotlib.pyplot as plt
 import numpy as np
-from numpy import fft
+import pylab
 
 PATHTOFILES = pathlib.Path("csv_files/")
+
+
+def thresholding_algo(y):
+    lag = 5  # average and std. are based on past 5 observations
+    threshold = 3.5  # signal when data point is 3.5 std. away from average
+    influence = 0.5  # between 0 (no influence) and 1 (full influence)
+
+    signals = np.zeros(len(y))
+    filteredY = np.array(y)
+    avgFilter = [0]*len(y)
+    stdFilter = [0]*len(y)
+    avgFilter[lag - 1] = np.mean(y[0:lag])
+    stdFilter[lag - 1] = np.std(y[0:lag])
+    for i in range(lag, len(y)):
+        if abs(y[i] - avgFilter[i-1]) > threshold * stdFilter [i-1]:
+            if y[i] > avgFilter[i-1]:
+                signals[i] = 1
+            else:
+                signals[i] = -1
+
+            filteredY[i] = influence * y[i] + (1 - influence) * filteredY[i-1]
+            avgFilter[i] = np.mean(filteredY[(i-lag+1):i+1])
+            stdFilter[i] = np.std(filteredY[(i-lag+1):i+1])
+        else:
+            signals[i] = 0
+            filteredY[i] = y[i]
+            avgFilter[i] = np.mean(filteredY[(i-lag+1):i+1])
+            stdFilter[i] = np.std(filteredY[(i-lag+1):i+1])
+
+    return dict(signals = np.asarray(signals),
+                avgFilter = np.asarray(avgFilter),
+                stdFilter = np.asarray(stdFilter))
 
 
 def smooth_moving_window(l, window_len=99, include_edges='On'):
@@ -41,10 +73,11 @@ def smooth_moving_window(l, window_len=99, include_edges='On'):
 
 
 def get_pulse(voltage_vals: list[float]):
+    dic = thresholding_algo(voltage_vals)
     pulse_interval: list = []
 
     for val in voltage_vals:
-        if val < -200:
+        if val < -350:
             pulse_interval.append(True)
         else:
             pulse_interval.append(False)
@@ -67,7 +100,7 @@ def get_pulse(voltage_vals: list[float]):
                     end_false = True
 
         if start_flag and end_false:
-            if interval['end'] - interval['start'] > 100:
+            if interval['end'] - interval['start'] > 150:
                 intervals.append(interval)
             start_flag = False
             end_false = False
@@ -132,7 +165,8 @@ def volt_in_intervals(data_dict):
     return data_dict
 
 
-def data_filter(data_dict):
+def data_filter(data_dict_in):
+    data_dict = data_dict_in
 
     for key in data_dict.keys():
         # filtr sumu  vystrelky hodnot
@@ -158,87 +192,21 @@ def data_filter(data_dict):
                     values_vol[i] = -730
         except:
             pass
+
         rng = get_pulse(data_dict[key]['voltage'])
         data_dict[key]['voltage_fit'] = smooth_moving_window(np.array(data_dict[key]['voltage']))
         data_dict[key]['pulse_ranges'] = rng
         data_dict[key]['pulse_number'] = len(rng)
 
-        data_dict = volt_in_intervals(data_dict)
+
 
     return data_dict
-
-
-def result():
-    data_dict_raw = load_files()
-    data_dict = data_filter(data_dict_raw)
-
-
-    peak_num = 0
-    peak_num_vol = 0
-    inter_0_50 = 0
-    inter_50_80 = 0
-    inter_80_120 = 0
-    inter_more_120 = 0
-
-    for key in data_dict.keys():
-        values = data_dict[key]['current']
-        val_vol = smooth_moving_window(data_dict[key]['voltage'])
-        # rozdeleni do intervalu
-        try:
-            for i in range(1, len(values)):
-                if values[i - 1] <= values[i]:
-                    if values[i] > values[i + 1]:
-                        peak_num += 1
-                        if values[i] <= 50:
-                            inter_0_50 += 1
-                        elif values[i] <= 80:
-                            inter_50_80 += 1
-                        elif values[i] <= 120:
-                            inter_80_120 += 1
-                        else:
-                            inter_more_120 += 1
-
-            for i in range(1, len(val_vol)):
-                if val_vol[i - 1] >= val_vol[i]:
-                    if val_vol[i] < val_vol[i + 1]:
-                        peak_num_vol += 1
-
-        except:
-            pass
-
-        values = np.array(data_dict[key]['current'])
-        times = np.array(data_dict[key]['time'])
-        spectrum = fft.fft(values)
-        freq = fft.fftfreq(len(spectrum))
-
-        #plt.plot(freq, abs(spectrum))
-
-        wave_plt = plt
-        wave_plt.figure(layout="constrained")
-        wave_plt.xlabel("")
-        wave_plt.ylabel("")
-
-        wave_plt.plot(times, values, label="label", marker='o')
-        print(f"{key}  numbers of peaks 0-50:{inter_0_50} 50-80:{inter_50_80} 80-120:{inter_80_120} 120+:{inter_more_120}")
-        # giving a title to my graph
-        wave_plt.title(key)
-
-        wave_plt.show()
-
-
-
-        x = times
-        y = data_dict[key]['voltage']
-        y1 = data_dict[key]['voltage_fit']
-        print(len(y), len(y1))
-        plt.plot(x, y)
-        plt.plot(x, y1, )
-        plt.show()
 
 
 def result2():
     data_dict_raw = load_files()
     data_dict = data_filter(data_dict_raw)
+    data_dict = volt_in_intervals(data_dict)
 
     for key in data_dict.keys():
         time = data_dict[key]['time']
